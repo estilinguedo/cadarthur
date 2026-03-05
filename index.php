f58247dcb677c253e4b846b8b42de9b02aacb6b7
Aluno <arthur.arge@gmail.com>
2026-03-05 17:10:32 -0300

    adicionado o campo CEP no formulário de cadastro

--- index.php
+++ index.php
@@ -13,69 +13,69 @@
     <!-- Adicionando Javascript -->
     <script>
 
-//         $(document).ready(function() {
+         $(document).ready(function() {
 
-//             function limpa_formulário_cep() {
-//                 // Limpa valores do formulário de cep.
-//                 $("#rua").val("");
-//                 $("#bairro").val("");
-//                 $("#cidade").val("");
-//                 $("#uf").val("");
-//                 $("#ibge").val("");
-//             }
+             function limpa_formulário_cep() {
+                // Limpa valores do formulário de cep.
+                $("#rua").val("");
+                $("#bairro").val("");
+                $("#cidade").val("");
+                $("#uf").val("");
+                $("#ibge").val("");
+            }
             
-//             //Quando o campo cep perde o foco.
-//             $("#cep").blur(function() {
+            //Quando o campo cep perde o foco.
+            $("#cep").blur(function() {
 
-//                 //Nova variável "cep" somente com dígitos.
-//                 var cep = $(this).val().replace(/\D/g, '');
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
 
-//                 //Verifica se campo cep possui valor informado.
-//                 if (cep != "") {
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
 
-//                     //Expressão regular para validar o CEP.
-//                     var validacep = /^[0-9]{8}$/;
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
 
-//                     //Valida o formato do CEP.
-//                     if(validacep.test(cep)) {
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
 
-//                         //Preenche os campos com "..." enquanto consulta webservice.
-//                         $("#rua").val("...");
-//                         $("#bairro").val("...");
-//                         $("#cidade").val("...");
-//                         $("#uf").val("...");
-//                         $("#ibge").val("...");
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#rua").val("...");
+                        $("#bairro").val("...");
+                        $("#cidade").val("...");
+                        $("#uf").val("...");
+                        $("#ibge").val("...");
 
-//                         //Consulta o webservice viacep.com.br/
-//                         $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
 
-//                             if (!("erro" in dados)) {
-//                                 //Atualiza os campos com os valores da consulta.
-//                                 $("#rua").val(dados.logradouro);
-//                                 $("#bairro").val(dados.bairro);
-//                                 $("#cidade").val(dados.localidade);
-//                                 $("#uf").val(dados.uf);
-//                                 $("#ibge").val(dados.ibge);
-//                             } //end if.
-//                             else {
-//                                 //CEP pesquisado não foi encontrado.
-//                                 limpa_formulário_cep();
-//                                 alert("CEP não encontrado.");
-//                             }
-//                         });
-//                     } //end if.
-//                     else {
-//                         //cep é inválido.
-//                         limpa_formulário_cep();
-//                         alert("Formato de CEP inválido.");
-//                     }
-//                 } //end if.
-//                 else {
-//                     //cep sem valor, limpa formulário.
-//                     limpa_formulário_cep();
-//                 }
-//             });
-//         });
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#rua").val(dados.logradouro);
+                                $("#bairro").val(dados.bairro);
+                                $("#cidade").val(dados.localidade);
+                                $("#uf").val(dados.uf);
+                                $("#ibge").val(dados.ibge);
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulário_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário_cep();
+                }
+            });
+        });
 
     </script>
     <script>
@@ -143,9 +143,21 @@
     <div id="cadastro">
         	<h2>Cadastro</h2>
             <form>
-                Email: <input type="email" name="emailcadastro" placeholder="fulano@gmail.com"> <br>
+            Email: <input type="email" name="emailcadastro" placeholder="fulano@gmail.com"> <br>
                 Nome: <input type="text" name="nomecadastro" placeholder="Fulano de tal"><br>
                 Senha: <input type="password" name="senhacadastro"><br>
+        <label>Cep:
+        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9" /></label><br />
+        <label>Rua:
+        <input name="rua" type="text" id="rua" size="60" /></label><br />
+        <label>Bairro:
+        <input name="bairro" type="text" id="bairro" size="40" /></label><br />
+        <label>Cidade:
+        <input name="cidade" type="text" id="cidade" size="40" /></label><br />
+        <label>Estado:
+        <input name="uf" type="text" id="uf" size="2" /></label><br />
+        <label>IBGE:
+        <input name="ibge" type="text" id="ibge" size="8" /></label><br /> 
             </form>
             <button type="button" id="botaocadastro">Cadastrar-me</button>
     </div>
